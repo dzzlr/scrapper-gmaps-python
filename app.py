0dfff3c1a4706354c46bd322cf8594a9d2e096c2
Fakhri Hassan Maulana <87844859+dzzlr@users.noreply.github.com>
2023-04-11 19:00:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,48 +6,19 @@ from scrapper.webdriver_config import WebDriver
 from scrapper.utils import process_date
 
 if __name__ == '__main__':
-    # f = open('metadata.json')
-    # metadata = json.load(f)
-
-    # location = metadata['data'][10]['name']
-    # location_url = metadata['data'][10]['url']
-    # reviews_count = metadata['data'][10]['reviews_count']
-
-    # f.close()
-
-    # loop_optimizer = 120 if reviews_count > 1000 else abs(int(reviews_count // 10) - 1)
-    # wd = WebDriver("C:\\edgedriver_win64\\msedgedriver.exe")
-    # wd.scrape(location, location_url, loop_optimizer)
-
-    md = MongoDBConfig('config.yaml')
-    # md.delete_all()
-
-    f = open("metadata.json")
+    f = open('metadata.json')
     metadata = json.load(f)
-
-    datas = metadata["data"]
-
+    datas = metadata['data']
     f.close()
-
-    count = 0
-    count_place = 0
+    
     for data in datas:
-
-        reviews_scrapped = 0
-        status = 'Not yet'
-        if (md.count_data({'location': data['name']}) > 0):
-            reviews_scrapped = md.count_data({'location': data['name']})
-            status = 'Done'
-            count_place += 1
-
-        result = {
-            'id': data['id'],
-            'name': data['name'],
-            'reviews_count': data['reviews_count'],
-            'reviews_scrapped': reviews_scrapped,
-            'status': status
-        }
-
-        count += reviews_scrapped
-        print(result)
-    print({'total_place_scrapped': count_place-1, 'total_reviews_scrapped': count})
+        if data['status'] == True:
+            location = data['name']
+            location_url = data['url']
+            reviews_count = data['reviews_count']
+            
+            loop_optimizer = 120 if reviews_count > 1000 else abs(int(reviews_count // 10) - 1)
+            wd = WebDriver("C:\\edgedriver_win64\\msedgedriver.exe")
+            wd.scrape(location, location_url, loop_optimizer)
+            
+        time.sleep(3)
